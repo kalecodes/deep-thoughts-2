ee48318df36282c4a17a855fbf9510433bbb842f
kalen510 <kalen.wiley@gmail.com>
2023-05-19 17:48:15 -0400

    sign JWT

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,6 +13,11 @@ const typeDefs = gql`
     friends: [User]
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -37,8 +42,8 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    login(email: String!, password: String!): User
-    addUser(username: String!, email: String!, password: String!): User
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
 
