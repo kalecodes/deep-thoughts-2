558ab4e0b5dfea45e547cb69160016fbd0f6882e
kalen510 <kalen.wiley@gmail.com>
2023-05-19 19:50:40 -0400

    connect front+back ends and thoughtlist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Home from './pages/Home';
+
+// establish a new link ot the GraphQL server at its /graphql endpoint
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql'
+});
+//instantiate the Apollo Client instance and create connection to endpoint
+const client = new ApolloClient({
+  link: httpLink,
+  // instantiate a new cache object
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+        <div className='container'>
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
