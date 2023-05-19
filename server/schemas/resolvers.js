9261b182d6a28c6c8a3e4c437ee657bf5f6244eb
kalen510 <kalen.wiley@gmail.com>
2023-05-19 18:30:34 -0400

    thought, reaction, friend mutations fixes #2

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,6 +16,7 @@ const resolvers = {
       
       throw new AuthenticationError('Not logged in');
     },
+
     users: async () => {
       return User.find()
         .select('-__v -password')
@@ -60,6 +61,51 @@ const resolvers = {
 
       const token = signToken(user);
       return { token, user };
+    },
+    
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const thought = await Thought.create({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+
+        return thought;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedThought = await Thought.findOneAndUpdate(
+          { _id: thoughtId },
+          { $push: { reactions: { reactionBody, username: context.user.username } } },
+          { new: true, runValidators: true }
+        );
+
+        return updatedThought;
+      }
+
+      throw new AuthenticationError('You ned ot be logged in!');
+    },
+
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          // use $addToSet to prevent duplicate entries
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate('friends');
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
     }
   }
 };
